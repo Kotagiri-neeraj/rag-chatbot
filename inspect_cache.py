b47ba72de8a06880c1de166ba12fc9d3c48d5e7b
Kotagiri Neeraj <142030042+Kotagiri-neeraj@users.noreply.github.com>
2026-06-10 10:53:06 +0530

    Deleted inspect_cache.py

--- inspect_cache.py
+++
@@ -1,8 +0,0 @@
-import os
-from pathlib import Path
-cache=os.getenv('TRANSFORMERS_CACHE') or os.getenv('HF_HOME')
-print('TRANSFORMERS_CACHE', cache)
-root=Path(cache) if cache else Path.home()/'.cache'/'huggingface'/'transformers'
-print('cache root', root)
-for p in root.rglob('*flan-t5-small*'):
-    print(p)
